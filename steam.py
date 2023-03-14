80b0cac3d2d28805dd3c9c3d6dd3a06a75b2eee7
ran-sama <31311154+ran-sama@users.noreply.github.com>
2023-03-14 17:21:20 +0100

    Adding demo key for account without games.

--- steam.py
+++ steam.py
@@ -8,7 +8,7 @@ char = '23456789BCDFGHJKMNPQRTVWXY'
 hex_time = ('%016x' % (int(time.time()) // 30))
 byte_time = bytes.fromhex(hex_time)
 
-digest = hmac.new(base64.b32decode('SHARED_SECRET_KEY'), byte_time, hashlib.sha1).digest()
+digest = hmac.new(base64.b32decode('E743CHKDEA44APEGLSDIUT2N4OY5NXTQ'), byte_time, hashlib.sha1).digest()
 begin = ord(digest[19:20]) & 0xF
 c_int = int.from_bytes((digest[begin:begin + 4]), "big") & 0x7fffffff
 
